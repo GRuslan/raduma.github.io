73b912d552aca6f30389851b1dcdb560b271f4b6
Asimo777 <garagonuch@gmail.com>
2016-06-09 21:45:59 +0300

    Changed code send email

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var app = angular.module("app", []);
+var l = null ;
 
 app.controller('LanguageCtrl', ['$scope', function LanguageController($scope) {
-  $scope.language = texts.sk;
+  l = $scope.language = texts.sk;
   $scope.setLanguage = function (language) {
     $scope.language = texts[language];
+    l = $scope.language ;
   };
 }]);
 
@@ -14,13 +16,17 @@ var texts = {
     your_name: "Ваше имя",
     submit: "Отправить",
     adress: "Наш адрес",
+    sent : "Отправлено!",
+    sent_error: "Действие временно недоступное! \r\n Попробуйте позже или отправьте ваши контакты на нашу электронную почту указанную в контактах!" 
   },
   sk: {
-    contact_us: "Kontaktujte nás",
-    phone_number: "Номер телефона",
+    contact_us: "Kontaktujte nás", 
+    phone_number: "Telefónne číslo",
     your_name: "Vaše meno",
     submit: "Predložiť",
     adress: "Naša adresa",
+    sent: "Poslal!",
+    sent_error: "Akcia je dočasne nedostupný!  \r\n Skúste to prosím neskôr alebo odoslať svoje kontaktné informácie na e-mailu uvedenom v kontaktoch"
   },
   en: {
     contact_us: "Contact us",
@@ -28,5 +34,31 @@ var texts = {
     your_name: "Your name",
     submit: "Submit",
     adress: "Our adress",
+    sent : "Sent!",
+    sent_error: "Action is temporarily unavailable! \r\n Please try again later or send your contact information on our e-mail indicated in contacts"
   }
 };
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+	var params = myform.serializeArray().reduce(function(obj, item) {
+      obj[item.name] = item.value;
+      return obj;
+  }, {});
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "mandrill";
+
+  var template_id = "feedback";
+  myform.find("button").text("Sending...");
+  emailjs.send(service_id,template_id,params)
+  	.then(function(){ 
+       myform.find("button").hide();
+       myform.find("#form_sent").show();;
+    }, function(err) {
+       alert(l.sent_error);
+    });
+  return false;
+});
